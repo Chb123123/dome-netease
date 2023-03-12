017c6c46c2ccdc80a5743845c1138fa5c0430ab3
Cai-Huaibin <100181878+Chb123123@users.noreply.github.com>
2023-03-12 16:27:14 +0800

    修改代码

---
+++ dist/js/659-legacy.7f4b3e9c.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkdemo_netease"]=self["webpackChunkdemo_netease"]||[]).push([[659],{8659:function(e,n,t){t.r(n),t.d(n,{default:function(){return i}});var s=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"singerListContainer"},[t("h1",[e._v("列表")])])}],u={},l=u,a=t(1001),c=(0,a.Z)(l,s,r,!1,null,null,null),i=c.exports}}]);
+//# sourceMappingURL=659-legacy.7f4b3e9c.js.map
--- src/router/index.js
+++ src/router/index.js
@@ -67,6 +67,7 @@ const routes = [
 
 const router = new VueRouter({
   mode: 'hash',
+  base: process.env.BASE_URL,
   routes
 })
 

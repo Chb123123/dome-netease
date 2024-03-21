5d330a7eb2eb2e65bed70ac48e1e9cd17d6bb12c
Cai-Huaibin <100181878+Chb123123@users.noreply.github.com>
2024-03-21 11:31:10 +0800

    打包编译文件

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const axios1 = axios.create({
   // baseURL: 'https://netease-eta.vercel.app'
-  baseURL: 'http://localhost:3000' // 本地执行
+  baseURL: 'http://121.41.122.125:3000' // 本地执行
 })
 
 export default axios1
